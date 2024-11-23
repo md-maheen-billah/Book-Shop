a9698a976d3ef8f544a4a467697ba5c0ae996f2c
md-maheen-billah <md.maheen.billah@icloud.com>
2024-11-23 15:39:03 +0100

    fixes

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const book_route_1 = require("./app/modules/book/book.route");
 const order_route_1 = require("./app/modules/order/order.route");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({ origin: ["http://localhost:5000"] }));
 app.use("/api/products", book_route_1.BookRoutes);
 app.use("/api/orders", order_route_1.OrderRoutes);
 app.get("/", (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import { BookRoutes } from "./app/modules/book/book.route";
 import { OrderRoutes } from "./app/modules/order/order.route";
-
+import cors from "cors";
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors({ origin: ["http://localhost:5000"] }));
 
 app.use("/api/products", BookRoutes);
 app.use("/api/orders", OrderRoutes);
